d620d220004ae3f0450228690a7ed2d66835e158
joppzhang <joppzhang@gmail.com>
2018-03-19 16:59:06 +0800

    :sad:

---
+++ sort_method/quick_sort.py
@@ -0,0 +1,11 @@
+#*_*coding=utf-8*_*
+
+def quick_sort(arr):
+    left =  arr[x for x in range(1,len(arr)):x<arr[0]]
+
+def main():
+    arr=[6,4,2,97,32,1,5,0,67,23,123,43]
+    print(quick_sort(arr))
+
+if __name__ == "__main__":
+    main()
